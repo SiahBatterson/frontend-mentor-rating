d70cb7ba0b09e68500d8905ed98c4ba139c59ea7
SiahBatterson <siahbatterson@gmail.com>
2024-12-02 17:51:29 -0500

    stores the value of the button to be used later now when the user pressed a rating button

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 submit_button = document.getElementById("Submission");
 var current_rating = 0;
 
-const allButtons = document.querySelectorAll('rating-button')
+const allButtons = document.querySelectorAll(".rating-button");
 allButtons.forEach((button) => {
-  button.addEventListener('click', function(ev) { 
-    console.log("rating pressed");
-  })
-})
+  button.addEventListener("click", function (ev) {
+    current_rating = ev.target.value;
+    console.log(`user rated: ${current_rating}`);
+  });
+});
 
 submit_button.addEventListener("click", myFunction);
 
 function myFunction() {
   console.log("triggered");
 }
-
-\
